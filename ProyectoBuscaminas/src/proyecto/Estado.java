bafc9ed7e8fa217284ec1a2fb9158bc3e5002356
VitorAzevedo76 <joao.76.azevedo@gmail.com>
2020-03-15 19:53:05 +0100

    He añadido la estructura de las casillas

---
+++ ProyectoBuscaminas/src/proyecto/Estado.java
@@ -0,0 +1,6 @@
+package proyecto;
+
+ public interface Estado {
+	public void hacerClickD(Casilla pCasilla);
+	public void hacerClickIz(Casilla pCasilla);
+}
---
+++ ProyectoBuscaminas/src/proyecto/Señalada.java
@@ -0,0 +1,17 @@
+package proyecto;
+
+public class Señalada implements Estado{
+
+	@Override
+	public void hacerClickD(Casilla pCasilla) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hacerClickIz(Casilla pCasilla) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
